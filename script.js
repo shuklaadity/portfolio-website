c4c6a77865f4d0d7deac600864c56c4529bcd78b
shuklaadity <aditya02shukla1@gmail.com>
2023-09-01 09:25:31 +0530

    portfolio website

---
+++ script.js
@@ -0,0 +1,172 @@
+
+// gate script
+
+function openg(){
+    var rg=document.getElementById("rightg");
+    var lg=document.getElementById("leftg");
+    
+    rg.classList.toggle("big1");
+    
+    lg.classList.toggle("big2");
+    console.log("cliked");
+   
+    
+    
+}
+
+
+// pages script 
+
+
+// contact us page styling = id page1
+
+
+
+function show(){
+    var pag = document.getElementById("page1");
+
+     pag.style.display="block";
+     pag.style.height="50vh";
+     pag.style.width="400px";
+     pag.style.margin="auto";
+     pag.style.transition=" 2s linear";
+     pag.style.transform="scale(2)";
+        
+        // pag.classList.toggle("big");
+}
+function down(){
+    var pag = document.getElementById("page1");
+
+     pag.style.transform="scale(0.0001)";
+
+     pag.style.height="300px";
+     pag.style.width="400px";
+     pag.style.transition="2s linear";
+}
+
+
+
+//project page script page id = page2
+
+function show2(){
+    var pag = document.getElementById("page2");
+
+     pag.style.display="block";
+     pag.style.top="250px";
+     pag.style.height="55vh";
+     pag.style.width="500px";
+     pag.style.transition=" 2s linear";
+     pag.style.transform="scale(2)";
+        
+        // pag.classList.toggle("big");
+}
+function down2(){
+    var pag = document.getElementById("page2");
+
+     pag.style.transform="scale(0.0001)";
+     
+     pag.style.height="300px";
+     pag.style.width="400px";
+     pag.style.transition="2s linear";
+}
+
+
+
+
+
+function show3(){
+    var pag = document.getElementById("page3");
+
+     pag.style.display="block";
+     pag.style.height="55vh";
+     pag.style.width="400px";
+
+     pag.style.transition=" 2s linear";
+     pag.style.transform="scale(2)";
+        
+        // pag.classList.toggle("big");
+}
+function down3(){
+    var pag = document.getElementById("page3");
+
+     pag.style.transform="scale(0.0001)";
+
+     pag.style.height="300px";
+     pag.style.width="400px";
+     pag.style.transition="2s linear";
+}
+
+
+
+
+
+function show4(){
+    var pag = document.getElementById("page4");
+
+     pag.style.display="block";
+     pag.style.height="55vh";
+     pag.style.width="500px";
+     pag.style.transition=" 2s linear";
+     pag.style.transform="scale(2)";
+        
+        // pag.classList.toggle("big");
+}
+function down4(){
+    var pag = document.getElementById("page4");
+
+     pag.style.transform="scale(0.0001)";
+
+     pag.style.height="300px";
+     pag.style.width="400px";
+     pag.style.transition="2s linear";
+}
+
+
+
+
+function show5(){
+    var pag = document.getElementById("page5");
+
+     pag.style.display="block";
+     pag.style.height="50vh";
+     pag.style.width="500px";
+     pag.style.transition=" 2s linear";
+     pag.style.transform="scale(2)";
+        
+        // pag.classList.toggle("big");
+}
+function down5(){
+    var pag = document.getElementById("page5");
+
+     pag.style.transform="scale(0.0001)";
+
+     pag.style.height="300px";
+     pag.style.width="400px";
+     pag.style.transition="2s linear";
+}
+
+
+function show6(){
+    var pag = document.getElementById("page6");
+
+     pag.style.display="block";
+     pag.style.top="250px";
+     pag.style.height="55vh";
+     pag.style.width="400px";
+     pag.style.transition=" 2s linear";
+     pag.style.transform="scale(2)";
+        
+        // pag.classList.toggle("big");
+}
+function down6(){
+    var pag = document.getElementById("page6");
+
+     pag.style.transform="scale(0.0001)";
+     
+     pag.style.height="300px";
+     pag.style.width="400px";
+     pag.style.transition="2s linear";
+}
+
+
+
